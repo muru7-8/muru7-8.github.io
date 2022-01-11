67d58e5ed0fb8f93cd1e9e7c4b0ebb74ff2e93d2
muru7-8 <67209244+muru7-8@users.noreply.github.com>
2022-01-11 18:57:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+  document.getElementById("header").innerHTML = 
+  `
+  <!-- HEADER -->
+  <div class="text-center mt-2">
+    <a href="/" class="font-bold text-2xl m-1 hover:text-gray-500 sm:text-5xl"> MURU 7.8</a>
+    <hr class="mt-2 w-3/4 m-auto">
+  </div>
+  
+  
+  <div class="grid grid-cols-2 gap-2 text-lg font-bold text-center mt-3 m-auto md:grid-cols-6">
+    <a href="index.html" class="hover:text-gray-500">PROYECTOS</a>
+    <a href="./acerca-de.html" class="hover:text-gray-500">ACERCA DE</a>
+    <a href="./quienes-somos.html" class="hover:text-gray-500">QUIENES SOMOS</a>
+    <a href="./bitacora.html" class="hover:text-gray-500">BITACORA</a>
+    <a href="./tienda.html" class="hover:text-gray-500">TIENDA</a>
+    <a href="./contacto.html" class="hover:text-gray-500">CONTACTO</a>
+
+  </div>
+  <hr class="mt-2 w-3/4 m-auto">
+  <!-- HEADER -->`
+  ;
+
+
+  document.getElementById("footer").innerHTML = 
+  `
+</div>
+<br>
+<hr class="mt-2 w-3/4 m-auto">
+<p class="text-center text-base text-gray-500">MURU 7.8  - 2022</p>
+`
+;
+
+
+};
+    
+
