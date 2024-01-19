40a4fd47d9f954e153a6393ddd64c298ccbadc1d
Nic Motta <nicolas.motta.contractor@gdevtools.com>
2024-01-19 18:08:42 -0300

    events

--- src/constants/content.js
+++ src/constants/content.js
@@ -57,3 +57,79 @@ export const CONTENT_INDEX = [
     href: "tradescantia",
   },
 ];
+
+export const CONTENT_EVENTS = [
+  {
+    year: "2024",
+    title: "Toda La Teoría Del Universo",
+    description:
+      "Cristalizaciones Namiki participated in the collective exhibition, Obras audiovisuales de las cosmogonías del tecnoceno, curated by Gabriel Munguía, for the Latin American Festival of Arts and Technologies “Toda La Teoría Del Universo”. Chile",
+    img: "../assets/img/tltdu.jpg",
+    link: "https://www.instagram.com/p/C1vOzAvx_OG/?hl=es&img_index=8",
+  },
+  {
+    year: "2023",
+    title: "Festival Biopoetrónika",
+    description:
+      "Respiro Namiki participated in the Festival  “Biopoetrónika” at the Teatrín de la Municipalidad de Arequipa, Perú.",
+    img: "../assets/img/biopoetronika.jpg",
+    link: "",
+  },
+  {
+    year: "2023",
+    title: "Coexistencias - MUNTREF, Tecnópolis",
+    description:
+      "Respiro Namiki participated in the exhibition collective “Coexistencias”. MUNTREF. Tecnópolis. Curated by Sebastián Pasquel.",
+    img: "../assets/img/coexistencias.jpg",
+    link: "",
+  },
+  {
+    year: "2021",
+    title: "Premio Fusión",
+    description:
+      "RespirO Namiki v0.1 received an honorable mention in the “Fusión” electronic arts competition. The exhibition was developed in the context of the “Noviembre Electrónico”, San Martín Cultural Center.",
+    img: "../assets/img/Fusion.jpg",
+    link: "https://fusion2020.frba.utn.edu.ar/agenda-cultural-noviembre-electronico-2020/",
+  },
+  {
+    year: "2021",
+    title: "Bienal de La Habana",
+    description:
+      "We participated in the Bienal de la Habana with the “RespirO Namiki” installation.",
+    img: "../assets/img/Bienalhabana.jpg",
+    link: "https://drive.google.com/file/d/1NziQ6xWlivvdU9GMtjBptRM38fyIfKUK/view",
+  },
+  {
+    year: "2021",
+    title:
+      "Proyecto Bios, Collective performance, Virtual platform  Mozilla Hub",
+    description:
+      "As a result fo the Covid-19 pandemic, art exhibitions were suspended in their in-person format. For this reason, during the “Proyecto Bios” event, we met the public on the Mozilla Hub virtual platform. In this context, we presented the performance “Ninfa y Achira”.",
+    img: "../assets/img/BiosPerformance.jpg",
+    link: "https://proyectobios.com/",
+  },
+  {
+    year: "2021",
+    title: "Proyecto Bios / Muestra Virtual",
+    description:
+      "We inaugurate our artgame “Universo Namiki” in the context of the virtual exhibition of the Projecto Bios",
+    img: "../assets/img/Biosproyecto.jpg",
+    link: "https://proyectobios.com/artistas/muru/",
+  },
+  {
+    year: "2021",
+    title: "Jornadas en Investigación en Arte",
+    description:
+      "The 6th Art Research Conference organized by the University of Costa Rica gave us the space to think about the artistic research developed in MURU 7.8. Invited by Susan Campos Fonseca to participate in the inaugural conference titled “Technological arts, Science and Decoloniality”, one of the members of MURU 7.8, Claudia Valente, reflected on the transversal axes that build our proposal.",
+    img: "../assets/img/JornadasCostaRica.jpg",
+    link: "https://www.ucr.ac.cr/actividades/2021/09/20/de-investigacion-en-artes.html",
+  },
+  {
+    year: "2020",
+    title: "Participación en el Proyecto Kawitu",
+    description:
+      "The video art project Can I caress you with a flower? It was created to accompany those who are healing in the Kawitu beds. This furniture design was created by a team from the Master's Degree in Technologies and Aesthetics of Electronic Arts at UNTREF coordinated by Mariela Yeregui. The proposal arises in the context of the Covid 19 pandemic and the furniture would be used in the event of overflowing hospital admission capacity. The videos we produced would be available to patients and accessed through a QR code printed on the bed.",
+    img: "../assets/img/kawitu.jpg",
+    link: "https://www.untref.edu.ar/mundountref/ecocamas-para-pacientes-con-covid-19-suman-solidaridad-empatia-arte-y-creatividad",
+  },
+];
